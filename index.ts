1ccad30ad0a35e084834e1bd69e5d1b08d402964
akhilesh.j@solutelabs.com <akiijadhav22@gmail.com>
2023-06-21 14:03:47 +0530

    Prepare for initial package publication

--- index.ts
+++
@@ -1,2 +0,0 @@
-export { default as CreateReport } from './pages/data-sources/[newDataSourceId]/reports';
-export { default as Accordion } from './components/accordions/accordion';
--- next.config.js
+++ next.config.js
@@ -7,14 +7,6 @@ const nextConfig = {
     defaultLocale: 'en',
     locales: ['en', 'ja'],
   },
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    config.module.rules.push({
-      test: /\.(ts|tsx)$/,
-      include: [path.join(__dirname, 'components'), path.join(__dirname, 'pages')],
-      use: [defaultLoaders.babel],
-    });
-    return config;
-  },
 };
 
 module.exports = nextConfig;
