11629d2bf72a88acdbd7d2bda76cabde62550664
akhilesh.j@solutelabs.com <akiijadhav22@gmail.com>
2023-06-23 13:53:46 +0530

    adding a page to export

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { default as Accordion } from '../components/accordions/accordion';
-export { default as ReportConfiguration } from '../components/new-reports/report-configuration';
+export { default as CreateReport } from '../pages/data-sources/[newDataSourceId]/reports';
 // export { default as ValidationTestList } from '../components/new-reports/validation-test-list';
 // export { default as MarkerSelection } from '../components/new-reports/markers-selection';
 // export { default as EditDataSourceGraph } from '../components/new-reports/edit-data-source-graph';
